c302bade2f6c3a0ae7319170b76aa72280b1e101
SiamSami <33563064+SiamSami@users.noreply.github.com>
2018-10-24 11:58:29 +0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,25 @@ int main(int argc, char *argv[])
 
     DApplication::loadDXcbPlugin();
     DApplication a(argc, argv);
-    a.setApplicationName("Deepin site blocker");
-    a.setApplicationVersion("Beta");
+    a.setWindowIcon(QIcon("/home/toor/Desktop/Programming/deepin-site-blocker-beta/Icon.png"));
+    a.setApplicationName("Site blocker");
+    a.setApplicationVersion("1.0");
+    a.setApplicationDescription("It is a simple site blocking program written in DTK (Deepin Tool Kit) & Qt");
+    a.setProductIcon(QIcon("/home/toor/Desktop/Programming/deepin-site-blocker-beta/Icon.png"));
+    a.setProductName("Site blocker");
+    a.setApplicationLicense("GNU GPL 3");
+    a.setApplicationHomePage("https://github.com/SiamSami/site-blocker");
+    a.setApplicationAcknowledgementPage("https://github.com/SiamSami/site-blocker");
+#ifdef Q_OS_LINUX
     if(geteuid() == 0){
         std::cout << "You are root\n";
     }else{
         std::cout << "Not root!\n";
+        //exit(1);
     }
+#endif
     siteblocker w;
-    w.setWindowTitle("Deepin site blocker");
+    w.setWindowTitle("Site blocker");
     w.show();
 
     return a.exec();
